43b24f4b6c01241532ac383fdd6139210b5d131f
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2018-01-02 15:40:28 +0530

    broadcast feature added

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,11 @@
 var socket = io();
-//method available to us because of above library
-//making request from client to server to open up web socket and keep that open
 
 socket.on('connect', function () {
-    console.log('connected to server');
-
-
-}); //socket.on listens to the events
+    console.log('Connected to server');
+});
 
 socket.on('disconnect', function () {
-    console.log('disconnected from server');
+    console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
--- server/server.js
+++ server/server.js
@@ -1,37 +1,50 @@
 const path = require('path');
-const express = require('express');
 const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
-
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-//lets you register event listener
 io.on('connection', (socket) => {
-  console.log('new user connected');
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Megha',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Megha',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
-    console.log('create message', message);
-    //io.emit emits message to every connection where socket.emit emits the message to single connection
+    console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
