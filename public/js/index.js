b90ae36f04db5fdc132ef38b97408e3a03c23de2
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2018-01-02 14:58:44 +0530

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+//method available to us because of above library
+//making request from client to server to open up web socket and keep that open
+
+socket.on('connect', function () {
+    console.log('connected to server');
+    socket.emit('createMessage', {
+        from: 'megha',
+        text: 'yeah this is the text'
+    });
+
+}); //socket.on listens to the events
+
+socket.on('disconnect', function () {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -17,6 +17,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  socket.emit('newMessage', {
+    from: 'thatsMe',
+    text: 'bye bye',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message)=>{
+    console.log('create message', message);
+  });
+
   socket.on('disconnect', ()=>{
     console.log('Client disconnected');
   });
