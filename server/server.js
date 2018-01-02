e034b2e339bf3bf65ec972c001e7166e938e4ed4
Megha-Hegde-Sirsi <meghahegde30@gmail.com>
2018-01-02 13:08:39 +0530

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+//lets you register event listener
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
-
-
-
-
-
-
-app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+  socket.on('disconnect', ()=>{
+    console.log('Client disconnected');
   });
+});
+
+server.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
